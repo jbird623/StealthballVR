e8e1e117854dda03cc78896d928b2602cada47db
JBird <cirigj@rpi.edu>
2017-03-05 20:00:51 -0500

    ++ Made robot off switch able to be flipped via ball or hand.

---
+++ StealthballVRProject/Assets/Scripts/RobotOffSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotOffSwitch : MonoBehaviour {
+
+    [SerializeField]
+    private Quaternion onPosition;
+    [SerializeField]
+    private Quaternion offPosition;
+    [SerializeField]
+    private float lerpTime;
+
+    private bool flipped;
+
+    void Awake () {
+        flipped = false;
+    }
+
+	void OnCollisionEnter () {
+        if (!flipped) {
+            TurnOff();
+            flipped = true;
+        }
+    }
+
+    void TurnOff () {
+        StartCoroutine(LerpToOffPosition());
+    }
+
+    IEnumerator LerpToOffPosition () {
+        float t = 0f;
+        while (t < lerpTime) {
+            transform.rotation = Quaternion.Lerp(onPosition, offPosition, t / lerpTime);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+}
