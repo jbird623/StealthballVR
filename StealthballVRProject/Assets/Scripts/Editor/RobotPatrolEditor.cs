845ba034293d7e4d8bc1e84ea8ea326beb7c9367
JBird <cirigj@rpi.edu>
2017-03-11 00:08:51 -0500

    . . Updated Robot Patrol editor script to have functionality for snapping the patrol route to the y-plane of the robot. . . Updated Robot Patrol editor script to use new JBirdEditor helper functions.

--- StealthballVRProject/Assets/Scripts/Editor/RobotPatrolEditor.cs
+++ StealthballVRProject/Assets/Scripts/Editor/RobotPatrolEditor.cs
@@ -2,25 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using JBirdEngine.EditorHelper;
 
 [CustomEditor(typeof(RobotPatrol))]
 public class RobotPatrolEditor : Editor {
 
     RobotPatrol targetPatrol;
 
+    int routeIndex = -1;
+
     void OnSceneGUI () {
         targetPatrol = (RobotPatrol)target;
 
         for (int i = 0; i < targetPatrol.route.Count; ++i) {
-            EditorGUI.BeginChangeCheck();
-            Vector3 newPoint = Handles.PositionHandle(targetPatrol.route[i], Quaternion.identity);
-
-            if (EditorGUI.EndChangeCheck()) {
-                Undo.RecordObject(target, "PatrolRouteChange");
-                targetPatrol.route[i] = newPoint;
+            if (routeIndex == i) {
+                this.UpdateListIndexViaHandle(ref targetPatrol.route, i, new PositionHandleDelegate(Handles.PositionHandle), targetPatrol.route[i], Quaternion.identity);
             }
 
             //Handles.DrawSphere();
+
+            if (routeIndex == i) {
+                Handles.color = Color.red;
+            }
+            else {
+                Handles.color = Color.blue;
+            }
+
+            if (Handles.Button(targetPatrol.route[i], Quaternion.identity, 0.35f, 0.42f, Handles.SphereCap)) {
+                routeIndex = i;
+                Repaint();
+            }
 
             Handles.color = Color.blue;
             Handles.DrawLine(targetPatrol.route[i], i < targetPatrol.route.Count - 1 ? targetPatrol.route[i+1] : targetPatrol.route[0]);
@@ -32,7 +43,17 @@ public class RobotPatrolEditor : Editor {
 
         targetPatrol = (RobotPatrol)target;
 
+        routeIndex = EditorGUILayout.IntField(routeIndex);
 
+        if (GUILayout.Button("Snap Patrol to Y-Plane")) {
+            this.HandleCustomEditorMethod(LockRouteToYPlane);
+        }
+    }
+
+    void LockRouteToYPlane () {
+        for (int i = 0; i < targetPatrol.route.Count; ++i) {
+            targetPatrol.route[i] = new Vector3(targetPatrol.route[i].x, targetPatrol.transform.position.y, targetPatrol.route[i].z);
+        }
     }
 
 }
